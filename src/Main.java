4759ec612b1c46b298b554b76c3d5f8498d218ef
kanari <christianaus13@gmail.com>
2024-02-08 13:51:24 +0800

    initialize project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+String [] names={"ryan","kanari","aus"};
+        String [] fruits= new String[3];
+
+        System.out.println(fruits[0]);
+        fruits[0]="banana";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(fruits[0]);
+
+    }
+}
